118fcb39a1b752bcdeb356fc5a76d29c22b24855
owlmoons <hello.projectweb@gmail.com>
2024-11-09 00:07:14 +0900

    Cap nhat cac update moi

---
+++ frontend/src/components/Element_Layout.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+/*As of now prop requires:
+Image source
+image alternative
+Element Title
+Element description*/
+const Element_Layout = (prop) => {
+  return (
+    <div className="card">
+      <img className="card-img-top" src={prop.src} alt={prop.alt} />
+      <div className="card-body">
+        <h5 className="card-title">{prop.title}</h5>
+        <p className="card-text">{prop.description}</p>
+        <p className="card-text">
+          <small className="text-muted">Last updated 3 mins ago</small>
+        </p>
+      </div>
+    </div>
+  );
+};
+export default Element_Layout;
